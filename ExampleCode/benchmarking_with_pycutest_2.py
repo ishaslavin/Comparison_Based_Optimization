19c954f966c67e0f383a9a12c73cebd603a222b4
Daniel Mckenzie <DanielMckenzie@users.noreply.github.com>
2022-04-25 14:30:51 -0700

    Working on pycutest benchmarking

--- ExampleCode/benchmarking_with_pycutest_2.py
+++ ExampleCode/benchmarking_with_pycutest_2.py
@@ -16,8 +16,87 @@ import pycutest
 from matplotlib import pyplot as plt
 from benchmarkfunctions import SparseQuadratic, MaxK
 from oracle import Oracle, Oracle_pycutest
-from Algorithms.stp_optimizer import STPOptimizer
-from Algorithms.gld_optimizer import GLDOptimizer
-from Algorithms.SignOPT2 import SignOPT
-from Algorithms.scobo_optimizer import SCOBOoptimizer
-from Algorithms.CMA_2 import CMA
+from pycutest_utils import run_STP_pycutest, run_GLD_pycutest, run_CMA_pycutest
+                           run_SCOBO_pycutest, run_signOPT_pycutest
+
+
+#==========================
+# 
+# Identify the relevant problems. Currently, we restrict to unconstrained 
+# problems of dimension less than 100
+#
+#==========================
+
+probs = pycutest.find_problems(constraints='U', userN=True)
+
+probs_under_100 = []
+
+for p in probs:
+    prob = pycutest.import_problem(p)
+    x0 = prob.x0
+    # only want <= 100.
+    if len(x0) <= 100:
+        probs_under_100.append(p)
+        
+
+#==========================
+# 
+# Initialize arrays to contain results. 
+#
+#==========================
+
+num_trials = 10
+num_problems = len(probs_under_100)
+       
+f_evals_STP = np.zeros(num_trials, num_problems)
+f_evals_GLD = np.zeros(num_trials, num_problems)
+f_evals_CMA = np.zeros(num_trials, num_problems)
+f_evals_SCOBO = np.zeros(num_trials, num_problems)
+f_evals_signOPT = np.zeros(num_trials, num_problems)
+
+
+#==========================
+# 
+# Run Experiment
+#
+#==========================
+
+prob_number = 0
+for problem in probs_under_100:
+    for i in range(num_trials):
+        p_invoke_ = pycutest.import_problem(problem)
+        x0 = p_invoke_.x0
+        print('dimension of problem: ', len(x0_invoke_))
+        function_budget_ = 100  # should make this bigger?
+        
+        #=========================== STP ====================================#
+        print('invoking STP in a loop....')
+        stp_f_vals, stp_function_evals = run_STP_pycutest(p_invoke_, 
+                                                          copy.copy(x0), 
+                                                          function_budget_,
+                                                          target_func_value)
+        f_evals_STP(i, prob_number) = stp_function_evals
+        print('\n')
+        
+        ## Finish rewriting remaining invocations.
+        # GLD.
+        print('invoking GLD in a loop....')
+        min2 = run_GLD_pycutest(p_invoke_, copy.copy(x0_invoke_), function_budget_)
+        GLD_err_list[i].append(min2)
+        print('\n')
+        # SignOPT.
+        print('invoking SignOPT in a loop....')
+        min3 = run_signOPT_pycutest(p_invoke_, copy.copy(x0_invoke_), function_budget_)
+        SignOPT_err_list[i].append(min3)
+        print('\n')
+        # SCOBO.
+        print('invoking SCOBO in a loop....')
+        min4 = run_SCOBO_pycutest(p_invoke_, copy.copy(x0_invoke_), function_budget_)
+        SCOBO_err_list[i].append(min4)
+        print('\n')
+        # CMA.
+        print('invoking CMA in a loop....')
+        min5 = run_CMA_pycutest(p_invoke_, copy.copy(x0_invoke_), function_budget_)
+        CMA_err_list[i].append(min5)
+        print('\n')
+
---
+++ ExampleCode/pycutest_utils.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 13:43:51 2022
+
+@author: danielmckenzie and ishaslavin
+
+Convenient functions for calling optimizers when working with pycutest.
+"""
+
+from oracle import Oracle, Oracle_pycutest
+from Algorithms.stp_optimizer import STPOptimizer
+from Algorithms.gld_optimizer import GLDOptimizer
+from Algorithms.SignOPT2 import SignOPT
+from Algorithms.scobo_optimizer import SCOBOoptimizer
+from Algorithms.CMA_2 import CMA
+
+def run_STP_pycutest(problem, x0, function_budget, target_func_value):
+    # STP.
+    print('RUNNING ALGORITHM STP....')
+    p = problem
+    direction_vector_type = 2  # uniform from sphere.
+    a_k = 0.001  # step-size.
+    n = len(x0)  # problem dimension.
+    '''
+    p.obj(x, Gradient=False) -> method which evaluates the function at x.
+    '''
+    oracle_stp = Oracle_pycutest(p.obj)  # comparison oracle.
+    stp = STPOptimizer(oracle_stp, direction_vector_type, x0, n, a_k, p.obj,
+                        2 * function_budget)
+  
+    # step
+    termination = False
+    while termination is False:
+        solution, func_value, termination = stp.step()
+        if func_value <= target_func_value:
+            termination = True
+            
+    return stp.f_vals, stp.function_evals
+
+def run_GLD_pycutest(problem, x0, function_budget, target_func_value):
+    # GLD.
+    print('RUNNING ALGORITHM GLD....')
+    p = problem
+    R_ = 1e-1
+    r_ = 1e-4
+    n = len(x0)  # problem dimension.
+    '''
+    p.obj(x, Gradient=False) -> method which evaluates the function at x.
+    '''
+    oracle_gld = Oracle_pycutest(p.obj)  # comparison oracle.
+    # GLD instance.
+    gld = GLDOptimizer(oracle_gld, p.obj, x0, R_, r_, 2 * function_budget)  # Why 2 times function budget?
+    
+    # step.
+    termination = False
+    while termination is False:
+        solution, func_value, termination = gld.step()
+        if func_value <= target_func_value:
+            termination = True
+            
+    return gld.f_vals, gld.function_evals
+
+
+def run_signOPT_pycutest(problem, x0, function_budget, target_func_value):
+    # SignOPT.
+    print('RUNNING ALGORITHM SIGNOPT....')
+    p = problem
+    m = 10
+    n = len(x0)  # problem dimension.
+    step_size = 0.2
+    r = 0.1
+    '''
+    p.obj(x, Gradient=False) -> method which evaluates the function at x.
+    '''
+    oracle_signopt = Oracle_pycutest(p.obj)  # comparison oracle.
+    # signOPT instance.
+    signopt = SignOPT(oracle_signopt, function_budget, x0, m, step_size,
+                      r, debug=False, function=p.obj)
+
+    # step.
+    termination = False
+    while termination is False:
+        solution, func_value, termination = signopt.step()
+        if func_value <= target_func_value:
+            termination = True
+            
+    return signopt.f_vals, signopt.function_evals
+
+def run_SCOBO_pycutest(problem, x0, function_budget, target_func_value):
+    # SCOBO.
+    print('RUNNING ALGORITHM SCOBO....')
+    p = problem
+    n = len(x0_scobo)  # problem dimension.
+    stepsize = 0.01
+    s_exact = 0.1*n
+    m_scobo = 4*s_exact
+    r = 0.1
+    '''
+    p.obj(x, Gradient=False) -> method which evaluates the function at x.
+    '''
+    oracle_scobo = Oracle_pycutest(p.obj)  # comparison oracle.
+    # SCOBO instance.
+    scobo = SCOBOoptimizer(oracle_scobo, stepsize, function_budget, x0, r,
+                           m_scobo, s_exact, objfunc=p.obj)
+
+    # step.
+    termination = False
+    while termination is False:
+        solution, func_value, termination = scobo.step()
+        if func_value <= target_func_value:
+            termination = True
+   
+    return scobo.f_vals, scobo.function_evals
+
+def run_CMA_pycutest(problem, x0, function_budget, target_func_value):
+    # CMA.
+    print('RUNNING ALGORITHM CMA....')
+    p = problem
+    r = 0.1
+    lam = 10
+    mu = 1
+    sigma = 0.5
+    '''
+    p.obj(x, Gradient=False) -> method which evaluates the function at x.
+    '''
+    oracle_cma = Oracle_pycutest(p.obj)  # comparison oracle.
+    # CMA instance.
+    all_func_vals = []
+    cma = CMA(oracle_cma, function_budget, x0, lam, mu, sigma, function=p.obj)
+    
+    # step.
+    termination = False
+    while termination is False:
+        solution, func_value, termination = cma.step()
+        if func_value[-1] <= target_func_value:
+            termination = True
+            
+    # step.
+    ## In below code, what is the error of convergence?
+    # for ij in range(function_budget):
+    #    val = cma1.step()
+    #    print(str(ij) + ': ' + str(val))
+    #    # handling error of convergence.
+    #    if ij > 1:
+    #        if np.abs(val - all_func_vals[-1]) < 1e-6:
+    #            all_func_vals.append(val)
+    #            break
+    #    all_func_vals.append(val)
+    
+    return cma.f_vals, cma.function_evals
